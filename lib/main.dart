ce5d3140a4096f1ffb537e56212dcf5c6be669eb
Luca <daguannoluca16@gmail.com>
2023-02-17 11:26:37 +0000

    Felix

--- lib/main.dart
+++ lib/main.dart
@@ -23,9 +23,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple, 
       ),
-      home: const MyHomePage(title: 'Schulplaner'),
+      home: const MyHomePage(title: 'Schulplaner 202X'),
     );
   }
 }

8898862a0fb356063ced80c9353b63d2cac9a9ca
Luca <daguannoluca16@gmail.com>
2023-03-07 08:08:44 +0000

    luca

--- lib/main.dart
+++ lib/main.dart
@@ -21,15 +21,66 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Screen')),
-      body: Column(
-        children: [
-          Text("Willkommen in der Schulplaner App"),
-          SizedBox(height: 20,),
-          
-        ],
-      )
-    );
+        appBar: AppBar(title: const Text('Home Screen')),
+        body: Column(
+          children: [
+            Text("Willkommen in der Schulplaner App"),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 100,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    color: Colors.purple[600],
+                    child: const Center(
+                        child: Text(
+                      'Item 1',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    )),
+                  ),
+                  Container(
+                    width: 200,
+                    color: Colors.purple[500],
+                    child: const Center(
+                        child: Text(
+                      'Item 2',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    )),
+                  ),
+                  Container(
+                    width: 200,
+                    color: Colors.purple[400],
+                    child: const Center(
+                        child: Text(
+                      'Item 3',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    )),
+                  ),
+                  Container(
+                    width: 200,
+                    color: Colors.purple[300],
+                    child: const Center(
+                        child: Text(
+                      'Item 4',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    )),
+                  ),
+                ],
+              ),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                _navigateToNextScreen(context);
+              },
+              backgroundColor: Colors.blue,
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ));
   }
 
   void _navigateToNextScreen(BuildContext context) {

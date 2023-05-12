32c9ae4795305723b1456a7950388c3380d1a387
Luca <daguannoluca16@gmail.com>
2023-05-12 10:49:01 +0000

    Felix

--- lib/model/notes.dart
+++ lib/model/notes.dart
@@ -1,6 +1,15 @@
 final String tableNotes = 'notes';
 
 class NoteFields {
+  static final List<String> values = [
+    id,
+    isImportant,
+    number,
+    title,
+    description,
+    time
+  ];
+
   static final String id = '_id';
   static final String isImportant = 'isImportant';
   static final String number = 'number';
@@ -42,6 +51,8 @@ class Note {
         description: description ?? this.description,
         createdTime: createdTime ?? this.createdTime,
       );
+
+  static Note fromJson(Map<String, Object?> json) => Note(id: json[NoteFields.id] as int?, isImportant: json[NoteFields.isImportant] == 1, number: json[NoteFields.number] as int, title: json[NoteFields.title] as String,description: json[NoteFields.description] as String, createdTime: DateTime.parse(json[NoteFields.time]as String),);
 
   Map<String, Object?> toJson() => {
         NoteFields.id: id,
